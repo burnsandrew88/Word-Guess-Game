faace9210a1e8c2b053e6af6650d3e1b21e5301c
Andrew Burns <andrewburns@Andrews-MacBook-Air.local>
2018-10-07 10:30:52 -0500

    New JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,120 +1,299 @@
-// variables
+// DOM Items
+var newGameButton = document.getElementById("new-game-button");
+var placeholders = document.getElementById("placeholders");
+var guessedLetters = document.getElementById("guessed-letters");
+var guessesLeft = document.getElementById("guesses-left");
+var wins = document.getElementById("win-count");
+var losses = document.getElementById("loss-count");
 
-var WordBank = ['cowboys', 'patriots', 'eagles', 'browns', 'raiders', 'bears', 'steelers', 'packersgross', 'giants', 'broncos', 'seahawks', 'chiefs', 'vikings', 'lions', 'chargers', 'rams', 'bucaneers', 'saints', 'cardinals', 'ravens', 'bengals', 'fortyniners', 'falcons', 'panthers', 'texans', 'jaguars', 'colts', 'titans']
 
-var PlayerGuess = [];
 
-var IncorrectLetterBank = "";
+// Game variables (WordBank, Wins, Losses, Picked Word, Guesses Left, game running, picked word placeholder, guessed letter bank, incorrect letter bank)
 
-var currentGuess = "";
-
-var guessesLeft = 10; 
+var WordBank = ['Cowboys', 'Patriots', 'Eagles', 'Browns', 'Raiders', 'Bears', 'Steelers', 'Packersgross', 'Giants', 'Broncos', 'Seahawks', 'Chiefs', 'Vikings', 'Lions', 'Chargers', 'Rams', 'Bucaneers', 'Saints', 'Cardinals', 'Ravens', 'Bengals', 'FortyNiners', 'Falcons', 'Panthers', 'Texans', 'Jaguars', 'Colts', 'Titans', 'Dolphins', 'Jets', 'Redskins', 'Bills']
 
 var wins = 0;
+var losses = 0;
+var guessesLeft = 10;
+var gameRunning = false;
+var pickedWord = ' ';
+var pickedWordPlaceholderArr = [];
+var guessedLetterBank = [];
+var incorrectLetterBank = [];
 
-var letterKeys = ("abcdefghijklmnopqrstuvwxyz").split("");
 
-// Chooses random item from the WordBank Array
+// new game function to reset all stats, pick new word, and create placeholders
 
-var randomInt =  WordBank [Math.floor(Math.random() * WordBank.length)];
+function newGame() {
+    // Resets all game info
+    gameRunning = true;
+    guessesLeft = 10;
+    guessedLetterBank = [];
+    incorrectLetterBank = [];
 
-var randomWord = randomInt;
 
-console.log (randomWord)
+    // pick a new word 
 
-// Functions
-var displayedWord = [];
-function answers(guess){
-    var randomWords = randomWord.split('')
-    for (var i = 0; i < randomWords.length; i++){
-        if (randomWords[i]=== guess){
-            displayedWord[i] === guess;
+    pickedWord = WordBank[Math.floor(Math.random() * WordBank.length)];
 
-        } else{
-            displayedWord[i] = "_";
+
+
+    // create the placeholders out of the word 
+
+    for (i = 0; i < pickedWord.length; i++) {
+        if (pickedWord[i] === ' ') {
+            pickedWordPlaceholderArr.push("  ");
+        } else {
+            pickedWordPlaceholderArr.push("_ ");
         }
-        
     }
-    console.log(displayedWord)
-}
-var placeholders = document.getElementById("placeholders");
-answers()
-console.log(PlayerGuess)
-placeholders.innerHTML = displayedWord.join('')
 
-// function Displayedlives(livesnumber){
-// document.getElementById("guesses-left").innerHTML = livesnumber;
+    // write all new game info to the DOM 
+    guessesLeft.textContent = guessesLeft;
+    placeholders.textContent = pickedWordPlaceholderArr.join('');
+    guessedLetters.textContent = incorrectLetterBank;
+}
+
+
+
+
+
+//letter guess function takes in the letter you pressed and see if its in the selected word
+
+function letterGuess(letter) {
+    console.log(letter);
+
+    if (gameRunning === true && guessedLetterBank.indexOf(letter) === -1) {
+        // Run Game Logic
+        guessedLetterBank.push(letter);
+
+        // guess if letter is in my picked word
+        for (var i = 0; i < pickedWord.length; i++) {
+            if (pickedWord[i].toLowerCase() === letter.toLowerCase()) {
+                pickedWordPlaceholderArr[i] = pickedWord[i];
+            }
+
+        }
+
+        placeholders.textContent = pickedWordPlaceholderArr.join('');
+        checkIncorrect(letter);
+    }
+    else {
+        if (!gameRunning) {
+            alert("The Game Isn't Running, Click on the Kick Button");
+        } else {
+            alert("You've Already Guessed this letter");
+        }
+    }
+
+}
+
+// check for incorrect (letter)
+
+function checkIncorrect(letter){
+    if(pickedWordPlaceholderArr.indexOf(letter.toLowerCase()) === -1 && pickedWordPlaceholderArr.indexOf(letter.toUpperCase()) === -1){
+        guessesLeft --;
+        incorrectLetterBank.push(letter);
+        guessedLetters.textContent = incorrectLetterBank.join(' ');
+        guessesLeft.textContent = guessesLeft;
+    }
+    checkLoss();
+}
+
+// checkLose
+function checkLoss (){
+    if(guessesLeft === 0){
+        losses++;
+        gameRunning = false; 
+        losses.textContent = losses;
+        placeholders.textContent = pickedWord;
+    }
+    checkWin();
+}
+//checkWin
+function checkWin(){
+    if(pickedWord.toLowerCase() === pickedWordPlaceholderArr.join('').toLowerCase()){
+        wins++;
+        gameRunning = false;
+        win.textContent = wins; 
+    }
+}
+
+// add event listener for new game button
+newGameButton.addEventListener("click", newGame);
+// event onkeyup event to trigger letter guess
+
+document.onkeyup = function(event){
+    if (event.keyCode >= 65 && event.keyCode <= 90){
+        letterGuess(event.key);
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// variables
+
+// var WordBank = ['cowboys', 'patriots', 'eagles', 'browns', 'raiders', 'bears', 'steelers', 'packersgross', 'giants', 'broncos', 'seahawks', 'chiefs', 'vikings', 'lions', 'chargers', 'rams', 'bucaneers', 'saints', 'cardinals', 'ravens', 'bengals', 'fortyniners', 'falcons', 'panthers', 'texans', 'jaguars', 'colts', 'titans']
+
+// var PlayerGuess = [];
+
+// var IncorrectLetterBank = "";
+
+// var currentGuess = "";
+
+// var guessesLeft = 10; 
+
+// var wins = 0;
+
+// var letterKeys = ("abcdefghijklmnopqrstuvwxyz").split("");
+
+// // Chooses random item from the WordBank Array
+
+// var randomInt =  WordBank [Math.floor(Math.random() * WordBank.length)];
+
+// var randomWord = randomInt;
+
+// console.log (randomWord)
+
+// // Functions
+// var displayedWord = [];
+// function answers(guess){
+//     var randomWords = randomWord.split('')
+//     for (var i = 0; i < randomWords.length; i++){
+//         if (randomWords[i]=== guess){
+//             displayedWord[i] === guess;
+//             displayedWord++;
+
+//         } else{
+//             displayedWord[i] = "_";
+//         }
+        
+//     }
+//     console.log(displayedWord)
+// }
+// var placeholders = document.getElementById("placeholders");
+// answers()
+// console.log(PlayerGuess)
+// placeholders.innerHTML = displayedWord.join('')
+
+// // function Displayedlives(livesnumber){
+// // document.getElementById("guesses-left").innerHTML = livesnumber;
+// // }
+
+// function UserDisplay (letter){
+
+//     document.getElementById("placeholders").textContent = letter;
 // }
 
-function UserDisplay (letter){
-
-    document.getElementById("placeholders").textContent = letter;
-}
-
-function winDisplay (number) {
-    document.getElementById("wins-count").textContent = number;
-}
+// function winDisplay (number) {
+//     document.getElementById("wins-count").textContent = number;
+// }
 
 
 
 
-var PlayerGuessArr = PlayerGuess;
+// var PlayerGuessArr = PlayerGuess;
 
-var GuessElements = [];
+// var GuessElements = [];
 
-// choose letters 
+// // choose letters 
 
-document.onkeyup = function (event){
-    var PlayerChoice = event.key;
-    // what key is it?
-    //     function 
-    // // is key correct?
-    //     function
-    // // if so - write to DOM
-    //     function
-    // // if wrong - decrement guess count
-    //     function
-    // // has word been guessed?
-    //     function
-    // // OR have guesses been exhausted?
-    //     function
-    if (letterKeys.includes(PlayerChoice)){
-        if (GuessElements.includes(PlayerChoice)){
-            alert ("That letter was already guessed!")
-        }  {
-             if (guessesLeft > 0) {
+// document.onkeyup = function (event){
+//     var PlayerChoice = event.key;
+//     // what key is it?
+//     //     function 
+//     // // is key correct?
+//     //     function
+//     // // if so - write to DOM
+//     //     function
+//     // // if wrong - decrement guess count
+//     //     function
+//     // // has word been guessed?
+//     //     function
+//     // // OR have guesses been exhausted?
+//     //     function
+//     if (letterKeys.includes(PlayerChoice)){
+//         if (GuessElements.includes(PlayerChoice)){
+//             alert ("That letter was already guessed!")
+//         }  {
+//              if (guessesLeft > 0) {
                 
-            if (randomWord.includes(PlayerChoice)){
-                // for (var z = 0; z < randomWord.length; z++){
-                // if (PlayerChoice === letterKeys[z]) {
-                    console.log('here')
-                    // GuessElements[z] = PlayerChoice;
-                    PlayerGuess.push(PlayerChoice);
-                    answers(PlayerGuess);
-                    placeholders.innerHTML = displayedWord.join('')
+//             if (randomWord.includes(PlayerChoice)){
+//                 // for (var z = 0; z < randomWord.length; z++){
+//                 // if (PlayerChoice === letterKeys[z]) {
+//                     console.log('here')
+//                     // GuessElements[z] = PlayerChoice;
+//                     PlayerGuess.push(PlayerChoice);
+//                     answers(PlayerGuess);
+//                     placeholders.innerHTML = displayedWord.join('')
                     
-                    // if(PlayerChoice.join("") === randomWord){
-                    //     alert ("YOU WIN!!!");
-                    //     wins++;
-                    //     winDisplay(number);
-                    // }
-                // }
-                // }
-            } else {
-                guessesLeft--;
-                GuessElements.push(PlayerChoice);
-                document.getElementById("guessed-letters").innerHTML = ("Incorrect Letters Guessed:" +  GuessElements)
-                document.getElementById("guesses-left").innerHTML = "You have " + guessesLeft;
-                if(guessesLeft === 0) {
-                    alert ("You have Lost...")
+//                     // if(PlayerChoice.join("") === randomWord){
+//                     //     alert ("YOU WIN!!!");
+//                     //     wins++;
+//                     //     winDisplay(number);
+//                     // }
+//                 // }
+//                 // }
+//             } else {
+//                 guessesLeft--;
+//                 GuessElements.push(PlayerChoice);
+//                 document.getElementById("guessed-letters").innerHTML = ("Incorrect Letters Guessed:" +  GuessElements)
+//                 document.getElementById("guesses-left").innerHTML = "You have " + guessesLeft;
+//                 if(guessesLeft === 0) {
+//                     alert ("You have Lost...")
 
-                }
-            }
-            }
-        }
-    }
+//                 }
+//             }
+//             }
+//         }
+//     }
 
-}
+// }
 
 
 
